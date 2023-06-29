93ca5c0b0f51a87c3559cab8c3bc17a36893e6d7
Deniz Orsel <denizonerorsel@gmail.com>
2023-06-29 11:57:23 +0200

    update save icon

--- src/components/Requestx.jsx
+++ src/components/Requestx.jsx
@@ -3,7 +3,7 @@ import "./Requestx.css";
 import Result from "./Result";
 import Query from "./Query";
 import { TbApi } from "react-icons/tb";
-import { IoClose } from "react-icons/io5";
+import { IoClose, IoSave } from "react-icons/io5";
 
 const Requestx = (props) => {
 
@@ -23,8 +23,8 @@ const RequestHandler = () => {
       <section className="High-Tabs">
         <TbApi className="icon-api" />
         <span>New Request</span>
-        
       </section>
+        <IoSave className="icon-save" />
       <div className="Request-sections">
         <Query onResult={ResultxHandler} />
         {Resultx ? <Result /> : null}
